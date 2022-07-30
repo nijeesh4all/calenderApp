0b6fd0ee0e6d57f51e6b5cf3109d7afa7fd2855d
Nijeesh Joshy <nijeeshjoshy@gmail.com>
2022-07-31 01:38:59 +0530

    added sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    devise_for :users, controllers: {
-      omniauth_callbacks: 'users/omniauth_callbacks'
-    }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   root 'home#index'
 end
