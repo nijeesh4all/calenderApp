c03428774d2923bf5b505959492c01e84ca69c6d
Nijeesh Joshy <nijeeshjoshy@gmail.com>
2022-07-31 15:21:46 +0530

    added hooks for update

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,22 +5,21 @@ class Event < ApplicationRecord
 
   validates_presence_of :start_date_time, :end_date_time, :title
 
-  after_create_commit :publish_event_to_google_calender,
-                      :register_event_webhook
+  after_create_commit :register_event_webhook
+  after_commit :publish_event_to_google_calender
 
   def register_event_webhook
     SubscribeGoogleEventsWebhookJob.perform_later(user_id, id) if event?
   end
 
-  private
-
-  def publish_event_to_google_calender
-    return if event?
-
-    PublishEventToGoogleJob.perform_later(id)
-  end
-
   def event?
     event.present?
   end
+
+  private
+
+  def publish_event_to_google_calender
+    PublishEventToGoogleJob.perform_later(id)
+  end
+
 end
